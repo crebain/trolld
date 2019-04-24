3a48790190ae097214830a1316cbcf974b53de0b
Troll Baggins <bilbas+troll@gmail.com>
2019-04-25 00:32:20 +0300

    Try pigpio

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const Gpio = require('onoff').Gpio;
-const radar = new Gpio(17, 'in', 'rising');
-radar.watch((err, value) => console.log('motion! ' + value + ';'));
+const Gpio = require('pigpio').Gpio;
+const radar = new Gpio(17, {
+  mode: Gpio.INPUT,
+  pullUpDown: Gpio.PUD_DOWN,
+  edge: Gpio.EITHER_EDGE
+});
+radar.on('interrupt', (level) => console.log(new Date().toISOString() + ' motion! ' + level + ';'));
 console.log('Started');
 
-process.on('SIGINT', () => {
-  radar.unexport();
-});
+//process.on('SIGINT', () => {
+//  radar.unexport();
+//});
